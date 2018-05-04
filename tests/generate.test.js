e5986c3898332ea9aca72ac869f77813e2b0df2b
seungprk@gmail.com <seungprk@gmail.com>
2018-05-04 14:49:17 -0700

    Set jest env and add test file for generateCore.js

---
+++ tests/generate.test.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const generate = require('../db/seeding/generateCore.js');
+
+const testFilePath = path.join(__dirname, 'test.json');
+
+beforeAll(done => generate('../../tests/test.json', 10, done));
+
+test('the file is created', (done) => {
+  fs.readFile(testFilePath, (err, data) => {
+    const json = JSON.parse(data);
+    expect(err).toBeNull();
+    expect(json.length).toBe(10);
+    expect(json[0].place_id).toBe(0);
+    expect(json[0].name).toBeDefined();
+    expect(json[0].price_level).toBeDefined();
+    expect(json[0].neighborhood).toBeDefined();
+    expect(json[0].city).toBeDefined();
+    expect(json[0].street).toBeDefined();
+    expect(json[0].rating).toBeDefined();
+    expect(json[0].reviews.length).toBeGreaterThan(0);
+    done();
+  });
+});
+
+afterAll(done => fs.unlink(testFilePath, done));
