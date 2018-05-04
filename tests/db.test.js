348468230d98521055fc12349abae7d15007392d
seungprk@gmail.com <seungprk@gmail.com>
2018-05-04 16:12:27 -0700

    Fix tests with data not being array

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,4 +1,5 @@
-const db = require('./../db/models/store.js');
+const mongoose = require('mongoose');
+const db = require('./../db/models/storeMongo.js');
 // Notes:
 // 1. Data must be seeded into Mongodb by running npm run seed.
 // 2. Mongodb must be started by using mongod from terminal
@@ -6,45 +7,31 @@ const db = require('./../db/models/store.js');
 
 var place_id = 0;
 
-test('the data is an array', () => {
-  expect.assertions(1);
-  return db.findOne(place_id).then((data) => {
-    expect(Array.isArray(data)).toBe(true);
-  });
-});
-
-test('the data is an array of length 1', () => {
-  expect.assertions(1);
-  return db.findOne(place_id).then((data) => {
-    expect(data.length).toBe(1);
-  });
-});
-
 test('the data has a place_id', () => {
   expect.assertions(1);
   return db.findOne(place_id).then(data => {
-    expect(data[0].place_id !== undefined).toBe(true);
+    expect(data.place_id !== undefined).toBe(true);
   });
 });
 
-test('the data[0].reviews is an array', () => {
+test('the data.reviews is an array', () => {
   expect.assertions(1);
   return db.findOne(place_id).then(data => {
-    expect(Array.isArray(data[0].reviews)).toBe(true);
+    expect(Array.isArray(data.reviews)).toBe(true);
   });
 });
 
-test('the data[0].reviews has at least 1 reviews', () => {
+test('the data.reviews has at least 1 reviews', () => {
   expect.assertions(1);
   return db.findOne(place_id).then(data => {
-    expect(data[0].reviews.length).toBeGreaterThanOrEqual(1);
+    expect(data.reviews.length).toBeGreaterThanOrEqual(1);
   });
 });
 
-test('the data[0].rating exists', () => {
+test('the data.rating exists', () => {
   expect.assertions(1);
   return db.findOne(place_id).then(data => {
-    expect(!!data[0].rating).toBe(true);
+    expect(!!data.rating).toBe(true);
   });
 });
 
