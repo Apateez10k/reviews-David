5710153ab93e2e77cbd00f8086445e141671bfc9
seungprk@gmail.com <seungprk@gmail.com>
2018-05-03 20:04:52 -0700

    Add schema file for postgres and ignore csv

--- db/postgresql/schema.sql
+++ db/postgresql/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE stores (
   neighborhood VARCHAR(50),
   city VARCHAR(50),
   street VARCHAR(50),
-  rating SMALLINT
+  rating REAL
 );
 
 CREATE TABLE reviews (
@@ -17,4 +17,16 @@ CREATE TABLE reviews (
   relative_time_description VARCHAR(20),
   text TEXT
 );
+
+CREATE TABLE stores_denormal (
+  place_id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  price_level SMALLINT,
+  neighborhood VARCHAR(50),
+  reviews JSON,
+  city VARCHAR(50),
+  street VARCHAR(50),
+  rating REAL
+);
+
 
