17ba2b02e2cf5741c2eb484ef17647df3d234e39
seungprk@gmail.com <seungprk@gmail.com>
2018-05-05 15:32:22 -0700

    Update snapshot and break out generate.js tests into multiple

--- __tests__/generate.test.js
+++ __tests__/generate.test.js
@@ -7,17 +7,70 @@ const testFilePath = path.join(__dirname, 'test.json');
 beforeAll(done => generate('../../__tests__/test.json', 10, done));
 
 test('the file is created', (done) => {
+  fs.readFile(testFilePath, (err) => {
+    expect(err).toBeNull();
+    done();
+  });
+});
+test('there should be 10 objects in the array', (done) => {
   fs.readFile(testFilePath, (err, data) => {
     const json = JSON.parse(data);
-    expect(err).toBeNull();
     expect(json.length).toBe(10);
+    done();
+  });
+});
+test('place_id should exist', (done) => {
+  fs.readFile(testFilePath, (err, data) => {
+    const json = JSON.parse(data);
     expect(json[0].place_id).toBe(0);
+    done();
+  });
+});
+test('name should exist', (done) => {
+  fs.readFile(testFilePath, (err, data) => {
+    const json = JSON.parse(data);
     expect(json[0].name).toBeDefined();
+    done();
+  });
+});
+test('price_level should exist', (done) => {
+  fs.readFile(testFilePath, (err, data) => {
+    const json = JSON.parse(data);
     expect(json[0].price_level).toBeDefined();
+    done();
+  });
+});
+test('neighborhood should exist', (done) => {
+  fs.readFile(testFilePath, (err, data) => {
+    const json = JSON.parse(data);
     expect(json[0].neighborhood).toBeDefined();
+    done();
+  });
+});
+test('city should exist', (done) => {
+  fs.readFile(testFilePath, (err, data) => {
+    const json = JSON.parse(data);
     expect(json[0].city).toBeDefined();
+    done();
+  });
+});
+test('street should exist', (done) => {
+  fs.readFile(testFilePath, (err, data) => {
+    const json = JSON.parse(data);
     expect(json[0].street).toBeDefined();
+    done();
+  });
+});
+test('rating should exist', (done) => {
+  fs.readFile(testFilePath, (err, data) => {
+    const json = JSON.parse(data);
     expect(json[0].rating).toBeDefined();
+    done();
+  });
+});
+test('reviews should have more than 0 items', (done) => {
+  fs.readFile(testFilePath, (err, data) => {
+    const json = JSON.parse(data);
     expect(json[0].reviews.length).toBeGreaterThan(0);
     done();
   });
