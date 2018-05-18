3bce695bb9e463e65019a181dad6b74860a65ee1
seungprk@gmail.com <seungprk@gmail.com>
2018-05-18 15:53:26 -0700

    Remove tests on deleted code and reset paths

--- __tests__/store.test.js
+++ __tests__/store.test.js
@@ -1,35 +1,40 @@
-const db = require('../db/models/storePostgres.js');
+const mongoose = require('mongoose');
+const db = require('./../db/models/store.js');
+// Notes:
+// 1. Data must be seeded into Mongodb by running npm run seed.
+// 2. Mongodb must be started by using mongod from terminal
+// 3. use database 'apateez'
 
-const id = 2;
+var place_id = 2;
 
 test('the data has a place_id', () => {
   expect.assertions(1);
-  return db.findOne(id).then((data) => {
+  return db.findOne(place_id).then(data => {
     expect(data.place_id !== undefined).toBe(true);
   });
 });
 
 test('the data.reviews is an array', () => {
   expect.assertions(1);
-  return db.findOne(id).then((data) => {
+  return db.findOne(place_id).then(data => {
     expect(Array.isArray(data.reviews)).toBe(true);
   });
 });
 
 test('the data.reviews has at least 1 reviews', () => {
   expect.assertions(1);
-  return db.findOne(id).then((data) => {
+  return db.findOne(place_id).then(data => {
     expect(data.reviews.length).toBeGreaterThanOrEqual(1);
   });
 });
 
 test('the data.rating exists', () => {
   expect.assertions(1);
-  return db.findOne(id).then((data) => {
+  return db.findOne(place_id).then(data => {
     expect(!!data.rating).toBe(true);
   });
 });
 
 afterAll(() => {
-  db.pool.end();
+  mongoose.disconnect();
 });
